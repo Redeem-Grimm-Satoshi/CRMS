744cd3970a1f270556122c8abe8a88d07e3f2613
Redeem-Grimm-Satoshi <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-09-09 21:37:24 +0100

    Dashboard Implemented

--- src/main/java/com/example/application/data/entity/Company.java
+++ src/main/java/com/example/application/data/entity/Company.java
@@ -4,17 +4,27 @@ import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.Formula;
+
 import java.util.LinkedList;
 import java.util.List;
 
 @Entity
 public class Company extends AbstractEntity {
+
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)")
+
+    private int employeeCount;
     @NotBlank
     private String name;
 
     @OneToMany(mappedBy = "company")
     @Nullable
     private List<Contact> employees = new LinkedList<>();
+
+    public int getEmployeeCount(){
+        return employeeCount;
+    }
 
     public String getName() {
         return name;
