fc065424e25ff69d56b4a1fdfacde79f9a84360f
Redeem-Grimm-Satoshi <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-09-13 22:58:27 +0100

    added unit test

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,5 +1,7 @@
 package com.example.application;
 
+import com.vaadin.base.devserver.BrowserLauncher;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
@@ -19,10 +21,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         name = "CRMS",
         shortName = "CRM",
         offlinePath="offline.html",
-        offlineResources = { "./images/offline.png"}
+        offlineResources = { "./images/offline.png",
+
+        }
+
 
 
 )
+
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
