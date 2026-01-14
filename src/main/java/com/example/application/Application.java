5576634892ef970a6c03ba46d53801b5aef53165
Redeem Grimm <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2026-01-13 21:12:54 -0500

    Clean up comments in Application.java
    Removed unnecessary comments in Application.java

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -9,10 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
- *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "crms")
